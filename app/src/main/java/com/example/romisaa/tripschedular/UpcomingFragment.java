7b1603dd729fa05f1083df91a505112c06abcbaf
Lsamy94 <mahmoud-samy94@hotmail.com>
2017-03-17 16:48:43 +0200

    upComing List here

--- app/src/main/java/com/example/romisaa/tripschedular/UpcomingFragment.java
+++ app/src/main/java/com/example/romisaa/tripschedular/UpcomingFragment.java
@@ -1,7 +1,12 @@
 package com.example.romisaa.tripschedular;
 
+import android.app.AlertDialog;
 import android.app.Fragment;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.icu.text.UnicodeSetSpanner;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -12,24 +17,43 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
+
+import java.util.ArrayList;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class UpcomingFragment extends Fragment {
 
-    String [ ]values={"samy","hhassan"};
-    ArrayAdapter adapter;
-    ListView mylistView;
+
+    ListArrayAdapter adapter;
+    SwipeMenuListView mylistView;
+    ArrayList <Trip> upCommingTrips;
+    DataBaseHandler handler;
+    int listpostion;
 
 
     public UpcomingFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        handler=new DataBaseHandler(getActivity());
+        upCommingTrips=new ArrayList<>();
+        upCommingTrips=handler.getAllUpcommingTrips();
+
+    }
 
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         getActivity().setTitle("Upcoming Trips");
+
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -37,8 +61,8 @@ public class UpcomingFragment extends Fragment {
         // Inflate the layout for this fragment
 
         View view= inflater.inflate(R.layout.fragment_upcoming, container, false);
-        mylistView= (ListView) view.findViewById(R.id.tripNames);
-        adapter=new ArrayAdapter(view.getContext(),R.layout.singlerow,R.id.textView,values);
+        mylistView= (SwipeMenuListView) view.findViewById(R.id.tripNames);
+        adapter=new ListArrayAdapter(view.getContext(),R.layout.singlerow,R.id.tripName,upCommingTrips);
         mylistView.setAdapter(adapter);
 
         mylistView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -47,10 +71,122 @@ public class UpcomingFragment extends Fragment {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
                 Intent intent=new Intent(view.getContext(),ViewTrip.class);
-                intent.putExtra("name",values[position]);
+                intent.putExtra("name",upCommingTrips.get(position));
                 startActivity(intent);
             }
         });
+
+        SwipeMenuCreator creator = new SwipeMenuCreator() {
+
+            @Override
+            public void create(SwipeMenu menu) {
+                // create "open" item
+                SwipeMenuItem startItem = new SwipeMenuItem(
+                        getActivity());
+                // set item background
+                startItem.setBackground(new ColorDrawable(Color.rgb(0x00, 0xff, 0x00)));
+                // set item width
+                startItem.setWidth((200));
+                // set item title
+                startItem.setTitle("Start");
+                // set item title fontsize
+                startItem.setTitleSize(18);
+                startItem.setTitleColor(Color.WHITE);
+                // add to menu
+                menu.addMenuItem(startItem);
+
+
+                // create "delete" item
+                SwipeMenuItem deleteItem = new SwipeMenuItem(
+                        getActivity());
+                // set item background
+                deleteItem.setBackground(new ColorDrawable(Color.rgb(0xF9,
+                        0x3F, 0x25)));
+                // set item width
+                deleteItem.setWidth((200));
+                // set item title
+                deleteItem.setTitle("delete");
+                // set item title fontsize
+                deleteItem.setTitleSize(18);
+                // set item title font color
+                deleteItem.setTitleColor(Color.WHITE);
+
+                // set a icon
+                //     deleteItem.setIcon(R.drawable.age);
+
+                // add to menu
+                menu.addMenuItem(deleteItem);
+            }
+        };
+        mylistView.setMenuCreator(creator);
+
+        mylistView.setOnSwipeListener(new SwipeMenuListView.OnSwipeListener() {
+
+            @Override
+            public void onSwipeStart(int position) {
+                // swipe start
+                listpostion=position;
+
+            }
+
+            @Override
+            public void onSwipeEnd(int position) {
+                // swipe end
+            }
+        });
+
+
+        mylistView.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
+                switch (index) {
+                    case 0:
+                        // start
+                        // l object m3ak ya abo 5al eml l enta 3ayzo fi
+                        Trip out= (Trip) mylistView.getItemAtPosition(listpostion);
+
+
+                       Toast.makeText(getActivity(),out.getName(),Toast.LENGTH_SHORT).show();
+                        break;
+                    case 1:
+                        // delete
+
+
+                        AlertDialog alertDialog = new AlertDialog.Builder(getActivity()).create();
+                        alertDialog.setTitle("Delete");
+                        alertDialog.setMessage("Do you Really want to delete that trip ");
+                        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "Yes",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        Trip deleted= (Trip) mylistView.getItemAtPosition(listpostion);
+                                        handler.deleteTrip(deleted.getId());
+                                        upCommingTrips.remove(listpostion);
+                                        adapter.notifyDataSetChanged();
+                                    }
+                                });
+                                alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "No",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                    }
+                                });
+
+                        alertDialog.show();
+                        // data.removeView(data.getAdapter().getView(listpostion,null,data));
+
+                       // Toast.makeText(getApplicationContext(),"delete done",Toast.LENGTH_SHORT).show();
+
+                        break;
+                }
+                // false : close the menu; true : not close the menu
+                return false;
+            }
+        });
+
+
+
+
+
         return view;
     }
 
