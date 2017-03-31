d4249d1d73df78e55841d852bad7669f2e589447
Lsamy94 <mahmoud-samy94@hotmail.com>
2017-03-31 17:41:39 +0200

    sidebar

--- app/src/main/java/com/example/romisaa/tripschedular/SignupActivity.java
+++ app/src/main/java/com/example/romisaa/tripschedular/SignupActivity.java
@@ -115,6 +115,7 @@ public class SignupActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
             }
         });
 
